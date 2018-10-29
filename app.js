ebe31265c0d4acebc7c2e9641fec4ba23203fe06
a-bort <bortron5000@gmail.com>
2018-10-28 23:28:25 -0400

    Wrong port... derp

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var express = require('express')
   , mysql = require('mysql');
 
 var app = express();
-var port = process.env.APP_PORT;
+var port = process.env.PORT || process.env.APP_PORT;
 
 var pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.PORT || process.env.DB_PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
